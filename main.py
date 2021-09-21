80e59d58202832df891716c96265de7bc8172101
Boss Perry (Pez) <88496461+bossperry08@users.noreply.github.com>
2021-09-21 08:01:11 +0700

    Add files via upload
    My first ever virtual assistant. Hope you like it! :))

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyttsx3
+
+#Basic virtual assistant
+engine = pyttsx3.init()
+engine.say("Hello, my name is Patrick." + " What's your name, mate?")
+engine.runAndWait()
+name = input()
+engine.say("Hello " + name + ", how are you, mate?")
+engine.runAndWait()
+status = input()
+engine.say("Oh, so you are " + status + " today, mate")
+engine.runAndWait()
+
+
+
+
+
+
