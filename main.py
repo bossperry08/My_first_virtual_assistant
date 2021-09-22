7afb4a943b7725b543b9e8867fa67c64830bcc7f
Boss Perry (Pez) <88496461+bossperry08@users.noreply.github.com>
2021-09-22 19:00:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyttsx3
 
 #Basic virtual assistant
 engine = pyttsx3.init()
-engine.say("Hello, my name is Patrick." + " What's your name, mate?")
+engine.say("Hello, my name is Apollo Patrick." + " What's your name, mate?")
 engine.runAndWait()
 name = input()
 engine.say("Hello " + name + ", how are you, mate?")
